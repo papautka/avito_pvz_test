ec15f9ef7de39d7e21fc821251dad897e3939426
Andrew Uteev <120500uas@gmail.com>
2025-04-25 12:46:13 +0300

    Разбил startapp.go на несколько подфункций. Убрал ненужные комментарии

---
+++ cmd/model.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"avito_pvz_test/internal/products"
+	"avito_pvz_test/internal/pvz"
+	"avito_pvz_test/internal/receptions"
+	"avito_pvz_test/internal/users"
+)
+
+type AllRepo struct {
+	UsersRepo     users.RepositoryUser
+	PvzRepo       pvz.RepositoryPvz
+	ReceptionRepo receptions.RepositoryReception
+	ProductRepo   products.RepositoryProduct
+}
+
+type AllService struct {
+	UsersService     users.ServiceUser
+	PvzService       pvz.ServicePvz
+	ReceptionService receptions.ServiceReception
+	ProductService   products.ServiceProduct
+}
+
+type AllHandler struct {
+	usersHandler     users.HandlerUser
+	pvzHandler       pvz.HandlerPvz
+	receptionHandler receptions.HandlerReception
+	productHandler   products.HandlerProduct
+}
--- cmd/startapp.go
+++ cmd/startapp.go
@@ -11,110 +11,88 @@ import (
 	"net/http"
 )
 
-// StartApp Старт приложения и инициализация зависимостей по слоям:
-// ┌────────────────────────────────────────────┐
-// │ Application Setup (StartApp)              │
-// ├────────────────────────────────────────────┤
-// │ 1. Загрузка конфигурации                   │
-// │ 2. Инициализация подключения к БД          │
-// │ 3. Создание репозиториев (data access)     │
-// │ 4. Создание сервисов (бизнес-логика)       │
-// │ 5. Инициализация хендлеров (transport)     │
-// │ 6. Настройка маршрутов и запуск сервера    │
-// └────────────────────────────────────────────┘
-//
-// Архитектура: config → db → repo → service → handler
 func StartApp() {
-	/*------------------------------------------------------------------------------*/
-
-	/* 1) Подгружаем файл config */
-	conf := config.NewConfig()
-
-	/*------------------------------------------------------------------------------*/
-
-	/* 2) работаем с базой данных */
-	db := database.CreateDb(conf)
-
-	/*------------------------------------------------------------------------------*/
-
-	/* 3) создаем репозитории на основе бд */
-	/* 3.1) репозиторий для User */
-	userRepo := users.NewRepoUser(db)
-
-	/* 3.2) репозиторий для PVZ */
-	pvzRepo := pvz.NewRepoPVZ(db)
-
-	/* 3.3) репозиторий для Reception */
-	receptionRepo := receptions.NewRepoRecep(db)
-
-	/* 3.4) репозиторий для Products */
-	productRepo := products.NewRepoProduct(db)
-
-	/*------------------------------------------------------------------------------*/
-
-	/* 4) создаем сервисы на основе репозиториев */
-	/* 4.1) сервис для User */
-	userService := users.NewServUser(userRepo, conf)
-
-	/* 4.2) сервис для PVZ */
-	pvzService := pvz.NewServPvz(pvzRepo)
-
-	/* 4.3) сервис для Reception */
-	receptionService := receptions.NewServReception(receptionRepo)
-
-	/* 4.4) сервис для Products */
-	productsService := products.NewServProduct(productRepo, receptionRepo)
-
-	/*------------------------------------------------------------------------------*/
-
-	/* 5) создаем Handlers на основе сервисов */
-
-	/* 5.1) handler для User */
-	userHandler := users.NewHandUser(userService)
-
-	/* 5.2) handler для PVZ */
-	pvzHandler := pvz.NewHandPvz(pvzService)
-
-	/* 5.3) handler для Reception */
-	receptionHandler := receptions.NewReceptionHandler(receptionService)
-
-	/* 5.4) handler для Products */
-	productHandler := products.NewHandProduct(productsService)
-
-	/*------------------------------------------------------------------------------*/
-	/* 6) создаем router в который будем класть все Handler */
-
-	router := http.NewServeMux()
-
-	/*------------------------------------------------------------------------------*/
-
-	/* 7) подключаем ручки к роутеру */
-
-	/* 7.1) ручки для User */
-	router.HandleFunc("POST /register", userHandler.CreateUser())
-	router.HandleFunc("POST /login", userHandler.AuthenticateUser())
-	router.HandleFunc("POST /dummyLogin", userHandler.GetTokenByRole())
-
-	/* 7.2) ручки для PVZ */
-	router.Handle("POST /pvz", midware.CheckRoleByToken(pvzHandler.CreatePVZ(), "moderator"))
-	router.Handle("POST /pvz/{pvzId}/close_last_reception", midware.CheckRoleByToken(pvzHandler.CloseLastReceptionByPvz(), "client"))
-
-	/* в работе */
-	router.HandleFunc("GET /pvz", pvzHandler.GetArrayPvz())
-	/* в работе */
-
-	/* 7.3) ручки для Reception */
-	router.Handle("POST /receptions", midware.CheckRoleByToken(receptionHandler.CreateReception(), "client"))
-
-	/* 7.4) ручки для Products */
-	router.Handle("POST /products", midware.CheckRoleByToken(productHandler.Create(), "client"))
-
-	// 8. Передаем в server наши ручки
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: router,
+		Handler: CreateRouter(),
+	}
+	server.ListenAndServe()
+}
+
+func loadConfig() *config.Config {
+	return config.NewConfig()
+}
+
+func loadDB(conf *config.Config) *database.Db {
+	return database.CreateDb(conf)
+}
+
+func loadRepository(db *database.Db) *AllRepo {
+	userRepo := users.NewRepoUser(db)
+	pvzRepo := pvz.NewRepoPVZ(db)
+	receptionRepo := receptions.NewRepoRecep(db)
+	productRepo := products.NewRepoProduct(db)
+	return &AllRepo{
+		UsersRepo:     userRepo,
+		PvzRepo:       pvzRepo,
+		ReceptionRepo: receptionRepo,
+		ProductRepo:   productRepo,
 	}
 
-	// 9. Запускаем server
-	server.ListenAndServe()
+}
+
+func loadService(a *AllRepo, c *config.Config) *AllService {
+	userService := users.NewServUser(a.UsersRepo, c)
+	pvzService := pvz.NewServPvz(a.PvzRepo)
+	receptionService := receptions.NewServReception(a.ReceptionRepo)
+	productsService := products.NewServProduct(a.ProductRepo, a.ReceptionRepo)
+	return &AllService{
+		userService,
+		pvzService,
+		receptionService,
+		productsService,
+	}
+}
+
+func loadHandlers(s *AllService) *AllHandler {
+	userHandler := users.NewHandUser(s.UsersService)
+	pvzHandler := pvz.NewHandPvz(s.PvzService)
+	receptionHandler := receptions.NewReceptionHandler(s.ReceptionService)
+	productHandler := products.NewHandProduct(s.ProductService)
+	return &AllHandler{
+		userHandler,
+		pvzHandler,
+		receptionHandler,
+		productHandler,
+	}
+
+}
+
+func connectHandlers(hs *AllHandler) *http.ServeMux {
+	router := http.NewServeMux()
+
+	router.HandleFunc("POST /register", hs.usersHandler.CreateUser())
+	router.HandleFunc("POST /login", hs.usersHandler.AuthenticateUser())
+	router.HandleFunc("POST /dummyLogin", hs.usersHandler.GetTokenByRole())
+
+	router.Handle("POST /pvz", midware.CheckRoleByToken(hs.pvzHandler.CreatePVZ(), "moderator"))
+	router.Handle("POST /pvz/{pvzId}/close_last_reception", midware.CheckRoleByToken(hs.pvzHandler.CloseLastReceptionByPvz(), "client"))
+	/* в работе */
+	router.HandleFunc("GET /pvz", hs.pvzHandler.GetArrayPvz())
+	/* в работе */
+
+	router.Handle("POST /receptions", midware.CheckRoleByToken(hs.receptionHandler.CreateReception(), "client"))
+
+	router.Handle("POST /products", midware.CheckRoleByToken(hs.productHandler.Create(), "client"))
+	return router
+}
+
+func CreateRouter() *http.ServeMux {
+	conf := loadConfig()
+	db := loadDB(conf)
+	repo := loadRepository(db)
+	services := loadService(repo, conf)
+	hs := loadHandlers(services)
+	router := connectHandlers(hs)
+	return router
 }
